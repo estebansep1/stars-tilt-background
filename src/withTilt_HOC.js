3ec53a46fe9710a31c15dadbbefd3de247e1a52f
Esteban Sepulveda <tebansd@icloud.com>
2024-06-19 13:56:15 -0700

    commented out HOC,not being used.

--- src/withTilt_HOC.js
+++ src/withTilt_HOC.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Tilt from './Tilt';
-import './Tilt.css';
+// import React from 'react';
+// import Tilt from './Tilt';
+// import './Tilt.css';
 
-const withTilt = (ComponentToRender) => 
-    class extends React.Component {
-        render() {
-            const { ...props } = this.props;
-            return (
-                <div className='hoc-wrapper'>
-                    <div className="tilt-wrapper">
-                        <Tilt />
-                    </div>
-                    <ComponentToRender {...props} />
-                </div>
-            );
-        }
-    };
+// const withTilt = (ComponentToRender) => 
+//     class extends React.Component {
+//         render() {
+//             const { ...props } = this.props;
+//             return (
+//                 <div className='hoc-wrapper'>
+//                     <div className="tilt-wrapper">
+//                         <Tilt />
+//                     </div>
+//                     <ComponentToRender {...props} />
+//                 </div>
+//             );
+//         }
+//     };
 
-export default withTilt;
+// export default withTilt;
